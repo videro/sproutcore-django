67a557bc75819662503abe35febec50d7ad1a417
VMplanet VMware Images <vmplanet@ubuntu.(none)>
2011-03-24 04:40:31 -0700

    Changed handling of fields and nestedRecords

--- record_attributes/django.js
+++ record_attributes/django.js
@@ -37,7 +37,8 @@ SC.RecordAttribute.registerTransform(Django.CharField, {
 
 /** @private */
 SC.RecordAttribute.registerTransform(Django.CommaSeparatedIntegerField, {
-  
+
+
   /** @private - 
     convert a comma-separated integer field to a string 
     allow null through as that will be checked separately
@@ -63,7 +64,7 @@ SC.RecordAttribute.registerTransform(Django.DateField, {
     var ret ;
     
     var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
-           "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\\.([0-9]+))?)?" +
+           " (([0-9]{2}):([0-9]{2})(:([0-9]{2})(\\.([0-9]+))?)?" +
            "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?",
         d      = str.match(new RegExp(regexp)),
         offset = 0,
@@ -109,17 +110,14 @@ SC.RecordAttribute.registerTransform(Django.DateField, {
 
 /** @private */
 Django.DATE_TIME_FIELD_REGEXP = new RegExp(
-  "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
-  "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\\.([0-9]+))?)?" +
-  "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?"
+  "([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})?"
 );
-
 /** @private */
 SC.RecordAttribute.registerTransform(Django.DateTimeField, {
   
   /** @private - convert a string to a Date */
   to: function(str, attr) {
-    var ret ;
+    /*var ret ;
     
     var d      = str.match(Django.DATE_TIME_FIELD_REGEXP),
         offset = 0,
@@ -143,6 +141,46 @@ SC.RecordAttribute.registerTransform(Django.DateTimeField, {
     ret = new Date();
     ret.setTime(Number(time));
     
+    return ret ;*/
+    
+    var ret ;
+    var d      = str.match(Django.DATE_TIME_FIELD_REGEXP),
+        offset = 0,
+        time ;
+    
+    var date = SC.DateTime.create({});
+    if (d)
+    {
+      
+      if (d[1]) 
+      { 
+        date.set('year',d[1]); 
+        
+      }
+      if (d[2]) { date.set('month',d[2] - 1); }
+      if (d[3]) { date.set('day',d[3]); }
+      
+      date = SC.DateTime.create({year:d[1], month:d[2], day:d[3]});
+      if (d[4]) { date.set('hour', d[4]); }
+      if (d[5]) { date.set('minute', d[5]); }
+      if (d[6]) { date.set('sec', d[6]); }
+      /*if (d[12]) { date.set('usec', Number("0." + d[12]) * 1000); }
+      if (d[14]) {
+         offset = (Number(d[16]) * 60) + Number(d[17]);
+         offset *= ((d[15] == '-') ? 1 : -1);
+      }*/
+      //console.log(date);
+      /*offset -= date.get('timezone');
+      time = (Number(date) + (offset * 60 * 1000));
+      */
+      
+
+      /*ret = new Date();
+      ret.setTime(Number(time));
+      */
+    }
+
+    ret = date;
     return ret ;
   },
   
@@ -154,6 +192,8 @@ SC.RecordAttribute.registerTransform(Django.DateTimeField, {
   
   /** @private - convert a date to a string */
   from: function(date) { 
+
+    /*
     var ret = this._dates[date.getTime()];
     if (ret) return ret ; 
     
@@ -173,14 +213,47 @@ SC.RecordAttribute.registerTransform(Django.DateTimeField, {
       tz) ;
     
     return ret ;
+    */
+
+    
+
+    var ret = this._dates[date.get('milliseconds')];
+    if (ret) return ret ; 
+    
+    // figure timezone
+    var zp = this._zeropad;
+        tz = 0-date.get('timezome')/60;
+    
+    tz = '';//(tz === 0) ? 'Z' : '%@:00'.fmt(zp(tz));
+    this._dates[date.get('milliseconds')] = ret = "%@-%@-%@ %@:%@:%@%@".fmt(
+      zp(date.get('year')),
+      zp(date.get('month')),
+      zp(date.get('day')),
+      zp(date.get('hour')),
+      zp(date.get('minute')),
+      zp(date.get('sec')),
+      tz) ;
+    return ret ;
   }
 });
 
 /** @private */
 SC.RecordAttribute.registerTransform(Django.DecimalField, {
   
+  /** @private - convert a Number to a Decimal */
+  from: function(str) {
+    // Check the String!!
+    str = String(str);
+    str = str.replace(',','.');
+    var str = parseFloat(str);
+
+    return String(str);
+  },
+
   /** @private - convert a decimal field to a Number */
   to: function(obj) {
+    
+    var num = String(obj);
     return SC.none(obj) ? null : Number(obj) ;
   }
   
@@ -221,6 +294,16 @@ SC.RecordAttribute.registerTransform(Django.FilePathField, {
 /** @private */
 SC.RecordAttribute.registerTransform(Django.FloatField, {
   
+  /** @private - convert a Number to a Decimal */
+  from: function(str) {
+    // Check the String!!
+    str = String(str);
+    str = str.replace(',','.');
+    var str = parseFloat(str);
+
+    return String(str);
+  },
+
   /** @private - convert a float field to a Number */
   to: function(obj) {
     return SC.none(obj) ? null : Number(obj) ;
@@ -247,6 +330,16 @@ SC.RecordAttribute.registerTransform(Django.ImageField, {
 /** @private */
 SC.RecordAttribute.registerTransform(Django.IntegerField, {
   
+  /** @private - convert a Number to a Decimal */
+  from: function(str) {
+    // Check the String!!
+    str = String(str);
+    str = str.replace(',','.');
+    var str = parseInt(str);
+
+    return String(str);
+  },
+
   /** @private - convert an integere field to a Number */
   to: function(obj) {
     return SC.none(obj) ? null : Number(obj) ;
@@ -283,6 +376,16 @@ SC.RecordAttribute.registerTransform(Django.NullBooleanField, {
 /** @private */
 SC.RecordAttribute.registerTransform(Django.PositiveIntegerField, {
   
+  /** @private - convert a Number to a Decimal */
+  from: function(str) {
+    // Check the String!!
+    str = String(str);
+    str = str.replace(',','.');
+    var str = parseInt(str);
+
+    return String(str);
+  },
+
   /** @private - convert a positive integer field to a Number */
   to: function(obj) {
     return SC.none(obj) ? null : Number(obj) ;
@@ -293,6 +396,16 @@ SC.RecordAttribute.registerTransform(Django.PositiveIntegerField, {
 /** @private */
 SC.RecordAttribute.registerTransform(Django.PositiveSmallIntegerField, {
   
+  /** @private - convert a Number to a Decimal */
+  from: function(str) {
+    // Check the String!!
+    str = String(str);
+    str = str.replace(',','.');
+    var str = parseInt(str);
+
+    return String(str);
+  },
+
   /** @private - convert a positive small integer field to a Number */
   to: function(obj) {
     return SC.none(obj) ? null : Number(obj) ;
@@ -319,6 +432,16 @@ SC.RecordAttribute.registerTransform(Django.SlugField, {
 /** @private */
 SC.RecordAttribute.registerTransform(Django.SmallIntegerField, {
   
+  /** @private - convert a Number to a Decimal */
+  from: function(str) {
+    // Check the String!!
+    str = String(str);
+    str = str.replace(',','.');
+    var str = parseInt(str);
+
+    return String(str);
+  },
+
   /** @private - convert a small integer field to a Number */
   to: function(obj) {
     return SC.none(obj) ? null : Number(obj) ;
@@ -350,25 +473,36 @@ SC.RecordAttribute.registerTransform(Django.TimeField, {
 
   /** @private - convert a string to a Date */
   to: function(str, attr) {
-    var ret ;
+    /*var ret ;
     
     var d      = str.match(Django.TIME_FIELD_REGEXP),
         offset = 0,
         date   = new Date(0, 0, 1),
         time ;
-    
     date.setHours(d[1]) ;
     date.setMinutes(d[2]) ;
     date.setSeconds(d[3]) ;
     
     return ret ;
+    */
+
+
+    var ret ;
+    
+    var d      = str.match(Django.TIME_FIELD_REGEXP),
+        offset = 0,
+        date   = null, //new Date(0, 0, 1),
+        time ;
+    date = SC.DateTime.create({hour: d[1], minute: d[2], sec: d[3]});
+
+    return ret ;
   },
   
   _dates: {},
-  
+   
   /** @private - convert a date to a string */
   from: function(date) { 
-    var ret = this._dates[date.getTime()];
+    /*var ret = this._dates[date.getTime()];
     if (ret) return ret ; 
     
     this._dates[date.getTime()] = ret = "%@:%@:%@".fmt(
@@ -377,7 +511,18 @@ SC.RecordAttribute.registerTransform(Django.TimeField, {
       zp(date.getSeconds())
     );
     
+    return ret ;*/
+    var ret = this._dates[date.get('usec')];
+    if (ret) return ret ; 
+    
+    this._dates[date.get('usec')] = ret = "%@:%@:%@".fmt(
+      zp(date.get('hour')),
+      zp(date.get('minute')),
+      zp(date.get('sec'))
+    );
+    
     return ret ;
+
   }
   
 });
